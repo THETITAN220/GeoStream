5c8d3fb316cac56d6d413b1a713573f797e7a576
THETITAN220 <raunakrose15@gmail.com>
2026-01-07 20:00:19 +0530

    ADDED: Consumer package

---
+++ backend/consumer/consumer.go
@@ -0,0 +1,50 @@
+package consumer
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/THETITAN220/GeoStream/proto/telemetry/v1"
+	"github.com/segmentio/kafka-go"
+	"google.golang.org/protobuf/proto"
+)
+
+type TelemetryConsumer struct {
+	reader *kafka.Reader
+}
+
+func NewTelemetryConsumer(brokers []string, topic, groupID string) *TelemetryConsumer {
+	return &TelemetryConsumer{
+		reader: kafka.NewReader(kafka.ReaderConfig{
+			Brokers:  brokers,
+			Topic:    topic,
+			GroupID:  groupID,
+			MinBytes: 10e3,
+			MaxBytes: 10e6,
+		}),
+	}
+}
+
+func (c *TelemetryConsumer) Start(ctx context.Context, dataChan chan<- *pb.SendDataRequest) {
+	defer c.reader.Close()
+
+	fmt.Println("Consumer started: listening for telemetry...")
+
+	for {
+		msg, err := c.reader.ReadMessage(ctx)
+		if err != nil {
+			log.Fatalf("Could not read the message: %v", err)
+		}
+
+		var telemetry pb.SendDataRequest
+		if err := proto.Unmarshal(msg.Value, &telemetry); err != nil {
+			log.Printf(" Failed to unmarshal telemetry: %v", err)
+			continue
+		}
+
+		dataChan <- &telemetry
+
+	}
+
+}
