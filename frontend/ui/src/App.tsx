f709875f8f25f1dec1b343838619963d89ab5f3b
THETITAN220 <raunakrose15@gmail.com>
2026-01-26 20:25:52 +0530

    trial push

--- frontend/ui/src/App.tsx
+++ frontend/ui/src/App.tsx
@@ -6,6 +6,8 @@ import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
+// But why?
+
 let DefaultIcon = L.icon({
     iconUrl: icon,
     shadowUrl: iconShadow,
